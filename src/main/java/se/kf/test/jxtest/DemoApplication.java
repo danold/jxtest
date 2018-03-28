98d1a4fd28d6e1d6e129ffdfa55850fa87346db4
Daniel Oldgren <daniel.oldgren@ffcg.se>
2018-03-28 13:04:14 +0200

    Initial import

---
+++ src/main/java/se/kf/test/jxtest/DemoApplication.java
@@ -0,0 +1,12 @@
+package se.kf.test.jxtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
